ae0b3b8eb7a31085d31c05ca33746a67a77109b1
Kéri Dominik <81580900+YungCarry@users.noreply.github.com>
2024-10-07 11:32:23 +0200

    Update Main.java

--- 1007/src/Main.java
+++ 1007/src/Main.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        prim();
+        fibo(10);
+    }
+//1. feladat
+    public static void prim() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Adja meg a csaoki gyártási számát: ");
@@ -25,6 +32,24 @@ public class Main {
         else {
             System.out.println("Gratulálunk, nyert :)");
         }
+    };
+
+    //2.feladat
+
+    public static int fibo(int n) {
+        ArrayList<Integer> szamok = new ArrayList<>();
+
+        szamok.add(0);
+        szamok.add(1);
+
+        for(int i = 2; i < n; i++){
+            int elozoelotti = szamok.get(i-2);
+            int elozo = szamok.get(i-1);
+            szamok.add(elozoelotti+elozo);
+        }
+
+        System.out.println("szamok: "+szamok);
+        return  szamok.get(n);
     }
 }
 
